b13d89f32d9fbbd5057c3c84c3b2b9cc3430108f
Zack Allen <zallen@cigital.com>
2013-06-04 14:03:30 -0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                  "Zack Allen"
+    email                 "zallen@cigital.com"
+    password              "Trustno1"
+    password_confirmation "Trustno1"
+  end
+end
